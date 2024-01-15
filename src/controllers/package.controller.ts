11c0ca43f02c208dc69c4a058c9cd381dc750fa6
Lakshan-Chamoditha-Perera <perera.alc2000@gmail.com>
2024-01-15 09:38:55 +0530

    get all packages

--- src/controllers/package.controller.ts
+++ src/controllers/package.controller.ts
@@ -3,3 +3,16 @@ import StandardResponse from "response/StandardResponse";
 import PackageModel from "model/package.model";
 import { IPackage } from "types/SchemaTypes";
 
+// getAll---------------------------------------------------------------------------------------------------------------------
+export const getAllPackages = async (
+    req: express.Request,
+    res: express.Response
+  ) => {
+    try {
+      let packages = await PackageModel.find();
+      res.send(new StandardResponse(200, "Packages found", packages));
+    } catch (err) {
+      res.send(new StandardResponse(500, "Something went wrong", null));
+    }
+  };
+  
